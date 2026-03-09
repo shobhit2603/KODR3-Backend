23c058dc16e5f73d54deec27c436ef4db324cba6
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-09 11:55:05 +0530

    Creating NotesApp with separate frontend and backend dir

--- class4/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-import mongoose from "mongoose";
-
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
