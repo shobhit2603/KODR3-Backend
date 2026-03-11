083fea7b674272d82c58f77c31490bdd48043ef3
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-11 11:16:29 +0530

    Learnt Authentication

--- class5/src/app.js
+++ class5/src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import morgan from "morgan";
-// import cors from "cors";
+import cors from "cors";
 import authRouter from './routes/auth.routes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(cors());
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 
