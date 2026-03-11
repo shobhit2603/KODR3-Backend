6aab7a46b15cd6b554f2b9c30e9afd63db01e7d5
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-11 12:49:40 +0530

    Completed Backend Class 6

--- class6/server.js
+++ class6/server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
